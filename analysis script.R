22bf9ebf97f7f8b2b2cb2128d54f949ccd126288
hannahstorysteinberg <hannahsteinberg@g.ucla.edu>
2025-05-12 13:48:50 -0700

    correlation

--- analysis script.R
+++ analysis script.R
@@ -2,5 +2,6 @@ library(readr)
 library(dplyr)
 df <- read_csv("JTmotility with blank rows removed.csv", col_names = TRUE)
 head(df)
-df_numeric <- select(df,-c(Patient_ID, Laterality))
-
+df_numeric <- select(df,-c(Patient_ID, Laterality, Global_Motility, Floor, Roof, Lateral, Medial))
+cor_mat <- cor(df_numeric, use = "pairwise.complete.obs")
+(sum(cor_mat > .6) - ncol(df_numeric))/2 # restriction and upgaze are highly negatively correlation 
