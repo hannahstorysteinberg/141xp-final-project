34aaf8f6afd5618b08e437a1b515e6ad27ad13bb
hannahstorysteinberg <hannahsteinberg@g.ucla.edu>
2025-06-08 23:24:27 -0700

    added comments and cleaned up the script

--- analysis script.R
+++ analysis script.R
@@ -5,15 +5,13 @@ library(lmerTest)
 library(tidyverse)
 library(pROC)
 library(ggplot2)
-# df <- read_csv("JTmotility with blank rows removed.csv", col_names = TRUE)
-# head(df)
-# df_numeric <- select(df,-c(Patient_ID, Laterality, Global_Motility, Floor, Roof, Lateral, Medial))
-# cor_mat <- cor(df_numeric, use = "pairwise.complete.obs")
-# (sum(cor_mat > .6) - ncol(df_numeric))/2 # restriction and upgaze are highly negatively correlation 
-# 
+library(sjPlot)
 
+# Read in Data
 df <- read_csv("Motility_final with blank rows removed.csv", col_names = TRUE)
 head(df)
+
+# Set Gobal Motility NAs to 0
 df$Global_Motility[is.na(df$Global_Motility)] <- 0
 
 #AD = MR
@@ -21,29 +19,28 @@ df$Global_Motility[is.na(df$Global_Motility)] <- 0
 #UP = SR
 #DOWN = IR
 
+
+# Calculate absolute scores, using a difference of fracture and control surface area
 df$SR_score <- df$fSR - df$cSR
 df$IR_score <- df$fIR - df$cIR
 df$LR_score <- df$fLR - df$cLR
 df$MR_score <- df$fMR - df$cMR
 
 
-df$sum_fracture_dich <- as.factor(ifelse(df$sum_fracture > 1, 1, 0))
-
 # Logistic Regression Model 1: Predicting Restrict Up
-model_up <- glm(restrict_Up ~ SR_score+sum_fracture_dich+Retrobulbar_hemorrhage, data= df, family = "binomial")
+model_up <- glm(restrict_Up ~ SR_score+sum_fracture+Retrobulbar_hemorrhage, data= df, family = "binomial")
 summary(model_up)
 
-#Upgaze+Downgaze+ADduction+ABduction+Retrobulbar_hemorrhage+Emphysema+fSR+fIR+fMR+fLR+restrict_Down+restrict_AB+restrict_AD+SR_size+IR_size+MR_size+LR_size+sum_fracture+muscle_bi
-# Logistic Regression Model 1: Predicting Restrict Down
-model_down <- glm(restrict_Down ~ IR_score+sum_fracture_dich+Retrobulbar_hemorrhage, data= df, family = "binomial")
+# Logistic Regression Model 2: Predicting Restrict Down
+model_down <- glm(restrict_Down ~ IR_score+sum_fracture+Retrobulbar_hemorrhage, data= df, family = "binomial")
 summary(model_down)
 
-# Logistic Regression Model 1: Predicting Restrict AB
-model_AB <- glm(restrict_AB ~ LR_score+sum_fracture_dich+Retrobulbar_hemorrhage, data= df, family = "binomial")
+# Logistic Regression Model 3: Predicting Restrict AB
+model_AB <- glm(restrict_AB ~ LR_score+sum_fracture+Retrobulbar_hemorrhage, data= df, family = "binomial")
 summary(model_AB)
 
-# Logistic Regression Model 1: Predicting Restrict AD
-model_AD <- glm(restrict_AD ~ MR_score+sum_fracture_dich+Retrobulbar_hemorrhage, data= df, family = "binomial")
+# Logistic Regression Model 4: Predicting Restrict AD
+model_AD <- glm(restrict_AD ~ MR_score+sum_fracture+Retrobulbar_hemorrhage, data= df, family = "binomial")
 summary(model_AD)
 
 
@@ -70,23 +67,22 @@ legend("bottomright", legend = c("Restrict Up", "Restrict Down", "Restrict AB", 
        col = c("blue", "red", "green", "purple"), lwd = 2)
 
 
-# without line at 1
-library(sjPlot)
+# Odds plots
 plot_model(model_up)
 plot_model(model_down)
 plot_model(model_AD)
 plot_model(model_AB)
 
 
-# with line at 1
-library(ggplot2)
 
-# replace model_down with the model you are using
-p <- plot_model(model_down, type = "est", transform = "exp")  # 'transform = "exp"' for odds ratios
-p + geom_hline(yintercept = 1, linetype = "dashed", color = "red")
+# Odds plots with line at 1
+plot_model(model_up, type = "est", transform = "exp") + geom_hline(yintercept = 1, linetype = "dashed", color = "red")
+plot_model(model_down, type = "est", transform = "exp") + geom_hline(yintercept = 1, linetype = "dashed", color = "red")
+plot_model(model_AB, type = "est", transform = "exp") + geom_hline(yintercept = 1, linetype = "dashed", color = "red")
+plot_model(model_AD, type = "est", transform = "exp") + geom_hline(yintercept = 1, linetype = "dashed", color = "red")
 
 
-# Logistic Regression Model 1: Predicting Any Restriction
+# Logistic Regression Model 5: Predicting Any Restriction (Not reported in the Final Report Slides)
 model_any <- glm(Restriction ~ cSR+cIR+cMR+cLR+fSR+fIR+fMR+fSR+sum_fracture+Retrobulbar_hemorrhage, data= df, family = "binomial")
 summary(model_any)
 
@@ -94,34 +90,3 @@ summary(model_any)
 # ROC/AUC for any restriction
 test_prob = predict(model_any, newdata = df, type = "response")
 test_roc = roc(df$Restriction ~ test_prob, plot = TRUE, print.auc = TRUE)
-
-# get threshold that render best results
-# coords(test_roc, "best", "threshold")
-# coords(test_roc)
-
-# df$Patient_ID <- as.factor(df$Patient_ID)
-# cor(df$Restriction, df$muscle_bi)
-# model <- glmer(Restriction~muscle_bi + (1|Patient_ID), data = df, family = "binomial")
-# summary(model)
-# 
-# 
-# table(df$Restriction)
-# table(df$muscle_bi)
-
-# 
-# # Logistic Regression Model 1: Predicting Restrict Up
-# model_up <- glm(restrict_Up ~ CSA_SR+CSA_IR+CSA_MR+CSA_LR+sum_fracture+Retrobulbar_hemorrhage, data= df, family = "binomial")
-# summary(model_up)
-# 
-# #Upgaze+Downgaze+ADduction+ABduction+Retrobulbar_hemorrhage+Emphysema+fSR+fIR+fMR+fLR+restrict_Down+restrict_AB+restrict_AD+SR_size+IR_size+MR_size+LR_size+sum_fracture+muscle_bi
-# # Logistic Regression Model 1: Predicting Restrict Down
-# model_down <- glm(restrict_Down ~ CSA_SR+CSA_IR+CSA_MR+CSA_LR+sum_fracture+Retrobulbar_hemorrhage, data= df, family = "binomial")
-# summary(model_down)
-# 
-# # Logistic Regression Model 1: Predicting Restrict AB
-# model_AB <- glm(restrict_AB ~ CSA_SR+CSA_IR+CSA_MR+CSA_LR+sum_fracture+Retrobulbar_hemorrhage, data= df, family = "binomial")
-# summary(model_AB)
-# 
-# # Logistic Regression Model 1: Predicting Restrict AD
-# model_AD <- glm(restrict_AD ~ CSA_SR+CSA_IR+CSA_MR+CSA_LR+sum_fracture+Retrobulbar_hemorrhage, data= df, family = "binomial")
-# summary(model_AD)
